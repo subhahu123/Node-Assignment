61234c648bc84301a74d9d8858abe74be16fccab
subhahu123 <jainsubhahu@gmail.com>
2019-06-14 02:21:23 -0700

    creating the api

---
+++ index.js
@@ -0,0 +1,67 @@
+const Express = require("express");
+const Mongoose = require("mongoose");
+const BodyParser = require("body-parser");
+
+var app = Express();
+
+Mongoose.connect("mongodb://zypher:subhahu123@ds337377.mlab.com:37377/user") ;
+
+const PersonModel = Mongoose.model("person", {
+    firstname: String,
+    lastname: String
+});
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+app.post("/person", async (request, response) => {
+    try {
+        var person = new PersonModel(request.body);
+        var result = await person.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+app.get("/people", async (request, response) => {
+    try {
+        var result = await PersonModel.find().exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+app.get("/person/:id", async (request, response) => {
+    try {
+        var person = await PersonModel.findById(request.params.id).exec();
+        console.log(person)
+        response.send(person);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+app.put("/person/:id", async (request, response) => {
+    try {
+        var person = await PersonModel.findById(request.params.id).exec();
+        person.set(request.body);
+        var result = await person.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+app.delete("/person/:id", async (request, response) => {
+    try {
+        var result = await PersonModel.deleteOne({ _id: request.params.id }).exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Listening at :3000...");
+});
+
+
+// mongodb://"zypher":"subhahu123"@ds337377.mlab.com:37377/user

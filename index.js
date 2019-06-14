2658cdd62345cc10ba3cec548b35c354c2e5bdf3
subhahu123 <jainsubhahu@gmail.com>
2019-06-14 04:10:38 -0700

    create post and get api

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const BodyParser = require("body-parser");
 
 var app = Express();
 
-Mongoose.connect("mongodb://zypher:subhahu123@ds337377.mlab.com:37377/user", {useNewUrlParser: true}) ;
+Mongoose.connect("mongodb://zypher:subhahu123@ds337377.mlab.com:37377/user") ;
 
-var userSchema = new Schema({
+var userSchema = new Mongoose.Schema({
     name: String,
     email: String,
     phone_number: String,
@@ -19,15 +19,17 @@ var userSchema = new Schema({
     interests: { type: [String], required: true }
 });
 
-const PersonModel = Mongoose.model("person", userSchema);
+const userModel = Mongoose.model("person", userSchema);
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
 app.post("/person", async (request, response) => {
     try {
-        var person = new PersonModel(request.body);
+        var person = new userModel(request.body);
+        var dd = request.body ;
         var result = await person.save();
+        console.log('Adding person: ' + JSON.stringify(dd));
         response.send(result);
     } catch (error) {
         response.status(500).send(error);
@@ -35,7 +37,7 @@ app.post("/person", async (request, response) => {
 });
 app.get("/people", async (request, response) => {
     try {
-        var result = await PersonModel.find().exec();
+        var result = await userModel.find().exec();
         response.send(result);
     } catch (error) {
         response.status(500).send(error);
@@ -43,8 +45,7 @@ app.get("/people", async (request, response) => {
 });
 app.get("/person/:id", async (request, response) => {
     try {
-        var person = await PersonModel.findById(request.params.id).exec();
-        console.log(person)
+        var person = await userModel.findById(request.params.id).exec();
         response.send(person);
     } catch (error) {
         response.status(500).send(error);
@@ -52,7 +53,7 @@ app.get("/person/:id", async (request, response) => {
 });
 app.put("/person/:id", async (request, response) => {
     try {
-        var person = await PersonModel.findById(request.params.id).exec();
+        var person = await userModel.findById(request.params.id).exec();
         person.set(request.body);
         var result = await person.save();
         response.send(result);
@@ -62,7 +63,7 @@ app.put("/person/:id", async (request, response) => {
 });
 app.delete("/person/:id", async (request, response) => {
     try {
-        var result = await PersonModel.deleteOne({ _id: request.params.id }).exec();
+        var result = await userModel.deleteOne({ _id: request.params.id }).exec();
         response.send(result);
     } catch (error) {
         response.status(500).send(error);
